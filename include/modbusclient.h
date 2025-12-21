8c488b8e979300e8ee7ce4e1f8f34440d77afdfc
phamxuantoan1996 <phamxuantoandc@gmail.com>
2025-12-21 08:04:30 +0000

    feat: Implement modbusclient interface

---
+++ include/modbusclient.h
@@ -0,0 +1,25 @@
+#ifndef MODBUSCLIENT_H
+#define MODBUSCLIENT_H
+
+#include <iostream>
+#include <stdint.h>
+#include <stdbool.h>
+#include <cerrno>
+#include <cstring>
+#include <modbus/modbus.h>
+#include <list>
+
+class modbusclient
+{
+private:
+    /* data */
+public:
+    virtual bool connect() = 0;
+    virtual void disconnect() = 0;
+    virtual std::list<uint16_t> readHoldingRegisters(uint8_t slave_id,uint16_t start_address,uint16_t num_of_reg) = 0;
+    virtual bool writeHoldingRegisters(uint8_t slave_id,uint16_t start_address,std::list<uint16_t> values) = 0;
+    virtual std::list<uint16_t> readInputRegisters(uint8_t slave_id,uint16_t start_address,uint16_t num_of_reg) = 0;
+    virtual ~modbusclient();
+};
+
+#endif
